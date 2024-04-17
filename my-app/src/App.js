c6b2a3e7f3484edb92652751e02a3f0c66171fdc
Kim Moberg <kimskogfelter@outlook.com>
2024-04-17 10:20:03 +0000

    conditional rendering class done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,11 @@
 import "./App.module.css";
-import NavBarSimple from './components/ClassComponentsChallenge';
-import EventsClass from "./components/EventsClass";
-import EventsFunctional from "./components/EventsFunctional";
-import EventBinding from "./components/EventBinding";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
 
 function App() {
   return <div className="App">
-     <EventBinding/>
+     <ConditionalRenderingClass/>
+     <ConditionalRenderingFunctional connected={false}/>
      
     
     </div>;
